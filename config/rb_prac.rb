a43b1835fe4fc4784c0d6c4d6d729bcb9fb6c688
Austin Poole <austinpoole@Austins-MacBook-Pro.local>
2015-05-06 20:11:05 -0400

    first commit

---
+++ config/rb_prac.rb
@@ -0,0 +1,22 @@
+is_array = ["x","y","z"]
+is_hash = {"x" => 1, "y" => 2, "z" => 3}
+
+hash = {"x" => 3, "y" => 2, "z" => 3}
+
+  hash["y"]
+
+array = ["x", "y", "z"]
+
+ array[0]
+
+ ice_cream = ["vanilla", "chocolate", "rocky road"]
+
+   ice_cream.push("strawberry")
+
+ atlanta_teams = {"baseball" => "braves", "football" => "falcons"}
+
+    atlanta_teams.store("basketball", "hawks")
+
+    num_array = [4, 6[2,7,3]]
+
+     num_array[2, 1]
